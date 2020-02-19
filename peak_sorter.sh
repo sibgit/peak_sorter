8e0d4fe462a1c35954be7081d3f5052a0eee617d
Robin Engler <robin.engler@sib.swiss>
2020-02-19 13:22:49 +0100

    peak_sorter: add check that input table has the ALTITUDE and PEAK columns

--- peak_sorter.sh
+++ peak_sorter.sh
@@ -9,6 +9,18 @@ INPUT_FILE='alpine_peaks_input.txt'
 OUTPUT_FILE='sorted_peaks.txt'
 echo "### Starting peak sorter script..."
 
+
+# Verify the data contains a column named "ALTITUDE", and a column named "PEAK"
+echo -n "### Running data integrity check..."
+if [[ $(head -n1 $INPUT_FILE | grep -c "ALTITUDE") -ne 1 ]]; then
+    echo "ALTITUDE column is missing in input data" && exit 1
+fi
+if [[ $(head -n1 $INPUT_FILE | grep -c "PEAK") -ne 1 ]]; then
+    echo "PEAK column is missing in input data" && exit 1
+fi
+echo "OK"
+
+
 # Retrieve number of the column that contains the peak elevation information.
 COL_NB=$(head -n1 $INPUT_FILE | tr '\t' '\n' | grep -n ALTITUDE | cut -f1 -d':')
 
