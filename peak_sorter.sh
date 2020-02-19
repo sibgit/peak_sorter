f3d8e2280010525ba29b0df63de8b7c2cd7daeaf
Robin Engler <robin.engler@sib.swiss>
2020-02-19 14:11:56 +0100

    peak_sorter: display name of highest peak when script completes

--- peak_sorter.sh
+++ peak_sorter.sh
@@ -4,7 +4,7 @@
 ### Sorts the 82 4000m peaks of the Alps from highest to lowest.
 ####################################################################################################
 
-# Input arguments.
+# Input arguments for script.
 INPUT_FILE='alpine_peaks_input.txt'
 OUTPUT_FILE='sorted_peaks.txt'
 echo "### Starting peak sorter script..."
@@ -12,8 +12,8 @@ echo "### Starting peak sorter script..."
 # Retrieve number of the column that contains the peak elevation information.
 COL_NB=$(head -n1 $INPUT_FILE | tr '\t' '\n' | grep -n ALTITUDE | cut -f1 -d':')
 
-# Sort peak list by summit elevation, from highest to lowest.
+# Sort alpine peak list by summit elevation, from highest to lowest.
 cat <( head -n1 $INPUT_FILE ) <( tail -n+2 $INPUT_FILE | sort -nr -k $COL_NB) > $OUTPUT_FILE
 echo "### Completed peak sorter script. Output file is: $OUTPUT_FILE"
-
+echo "### The highest peak in the Alps is: $( head -n2 $OUTPUT_FILE | tail -n1 | cut -f1,2 )"
 ####################################################################################################
